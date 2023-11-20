a2d577c6e9c7da34f7b9a7676abe322a25cb6744
Cameron Skerritt <122690042+Cameron-Skerritt@users.noreply.github.com>
2023-11-20 18:19:49 +0000

    Update hash.py

--- Hash Brute forcing/hash.py
+++ Hash Brute forcing/hash.py
@@ -1,9 +1,4 @@
 import hashlib
-import pyfiglet
-
-ascii_banner = pyfiglet.figlet_format("Python 4 Pentesters \n HASH CRACKER")
-print(ascii_banner)
-
 wordlist_location = str(input('Enter wordlist file: '))
 hash_input = str(input('Enter hash to be cracked: '))
 
